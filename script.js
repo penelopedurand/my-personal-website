1b2911488c9e569d71937e63ad312e5ae295baea
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-03-24 14:11:17 -0400

    first edit of text and image

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
+h2.textContent = "🍗 Feeding Simulator 🥩";
 document.querySelector("body").appendChild(h2);
