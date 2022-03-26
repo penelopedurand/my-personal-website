278d8ef83e6e3bc338a6f41c2f52dd1d3c14112d
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-03-26 14:48:28 -0400

    Moving Bowl

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const h2 = document.createElement("h2");
-h2.textContent = "🍗 Feeding Simulator 🥩";
-document.querySelector("body").appendChild(h2);
+
+const EL_wetfood = document.querySelector("#wetfood");
+const pos = {x:0, y:0};
+
+document.addEventListener("keydown", (ev) => {
+    const dir = (ev.key.match(/(?<=^Arrow)\w+/)||[])[0];
+    if (!dir) return; // Not an arrow key.
+  
+  ev.preventDefault(); // Prevent Browser scroll if overflow
+
+  ({
+    Left:  () => pos.x -= 5,
+    Right: () => pos.x += 5,
+    Up:    () => pos.y -= 5,
+    Down:  () => pos.y += 5,
+  }[dir])(); 
+  
+  EL_wetfood.style.transform = `translate(${pos.x}px, ${pos.y}px)`
+});
